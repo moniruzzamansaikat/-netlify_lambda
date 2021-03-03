d000630d6ec33aa42f989a289108cfb5e1550e70
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2021-03-03 22:55:35 +0600

    fix client

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-const fetchUsers = async () => {
-  const res = await fetch('/.netlify/functions/getusers')
-  const data = await res.json()
-  return data
-}
+fetch('/.netlify/functions/getusers')
+  .then((res) => res.json())
+  .then((data) => {
+    const usersList = document.querySelector('#users')
 
-fetchUsers().then((data) => {
-  const usersList = document.querySelector('#users')
+    data.forEach((user) => {
+      const li = document.createElement('li')
+      li.className = 'list-group-item'
 
-  data.forEach((user) => {
-    const li = document.createElement('li')
-    li.className = 'list-group-item'
-
-    const link = document.createElement('a')
-    link.appendChild(document.createTextNode(user.login))
-    link.href = user.html_url
-    link.target = '_blank'
-    li.appendChild(link)
-    usersList.appendChild(li)
+      const link = document.createElement('a')
+      link.appendChild(document.createTextNode(user.login))
+      link.href = user.html_url
+      link.target = '_blank'
+      li.appendChild(link)
+      usersList.appendChild(li)
+    })
   })
-})
