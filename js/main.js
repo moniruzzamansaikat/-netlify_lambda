fd9d912c1f7aca8d203896875c50b01107818cf7
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2021-03-03 22:40:39 +0600

    fix url

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () =>
-  await (await fetch('.netlify/functions/getusers')).json()
+  await (await fetch('/.netlify/functions/getusers')).json()
 
 fetchUsers().then((data) => {
   const usersList = document.querySelector('#users')
